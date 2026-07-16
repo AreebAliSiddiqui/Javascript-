15644b8df61d88011b2fbfeb65bf4c203ddf27d6
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-07-16 22:22:13 +0500

    assignment4

---
+++ assignment4/app.js
@@ -0,0 +1,17 @@
+// Question 1
+// Create one variable.
+
+let num = 10;
+let newNum = num++;
+console.log(newNum);
+console.log(num);
+
+// Question 2
+// Create one variable.
+
+let count = 20;
+let newCount = count--;
+console.log(newCount);
+console.log(count);
+
+
