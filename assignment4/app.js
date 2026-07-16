e6d67f962b186fd6c758a1799593eb113dd435dd
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-07-16 22:44:19 +0500

    updated

--- assignment4/app.js
+++ assignment4/app.js
@@ -14,4 +14,28 @@ let newCount = count--;
 console.log(newCount);
 console.log(count);
 
+// Question 2
+// Create one variable.
 
+let age = 18;
+let newAge = ++age;
+console.log(newAge);
+console.log(age);
+
+// Question 2
+// Create one variable.
+
+let marks = 95;
+let newMarks = --marks;
+console.log(newMarks);
+console.log(marks);
+
+
+// Question 5
+// Predict the output first, then run the code.
+
+let x = 5;
+console.log(x++);
+// prediction is x=5
+console.log(x);
+// prediction is x=6
