caa47a53d93de2e3e47fca955e62ba1bc22fc0d5
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-07-19 21:31:32 +0500

    assignment5

---
+++ assignment5/app.js
@@ -0,0 +1,53 @@
+// Question 1
+// Create two variables.
+
+let firstName = "Areeb ";
+let lastName = "Ali Siddiqui";
+let fullName = firstName + lastName ;
+console.log(fullName);
+
+// Question 2
+// Create one variable.
+
+let city = "I live in Karachi";
+console.log(city);
+city = "Islamabad";
+console.log(city);
+
+// Question 3
+// Use promp()
+
+let userName = prompt("Enter your name");
+console.log(userName);
+
+// Question 3
+// Use prompt().
+
+let userFirstName = prompt("Enter your first name");
+let userLastName = prompt("Enter your last name");
+console.log(userFirstName +" "+ userLastName);
+
+// Question 5
+// Use prompt().
+
+var userColor = prompt("Enter your fav color");
+console.log(userColor);
+
+// Question 6
+// Use two variables.
+
+let country = " Pakistan";
+let city2 = "I live in karachi,";
+console.log(city2 + country);
+
+// Question 7
+// Use three prompts.
+
+let name1 = prompt("Enter your name");
+let country2 = prompt("Enter your country");
+let city3 = prompt("Enter your city");
+let greeting = "hello ";
+let welcome = "Welcome to "
+console.log(greeting + name1 + "!");
+console.log(welcome + city3 + ", " + country2);
+
