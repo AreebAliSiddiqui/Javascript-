cdd05e1fe743947f72f8f89ab451d33910d3c28e
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-07-29 04:06:09 +0500

    assignment6

---
+++ assignment6/app.js
@@ -0,0 +1,64 @@
+// Question 1
+// Create the following variables and print them in the console.
+
+let firstName = "Areeb";
+let lastName = " Ali Siddiqui";
+let age = 19;
+let city = "karachi";
+console.log(firstName + lastName);
+console.log("Age:" + age +", "+ "City:" + city);
+
+// Question 2
+// Create two number variables.
+
+let num1 = 50;
+let num2 = 10;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+// Question 3
+// Create one variable.
+
+let count = 10;
+console.log(count++);
+console.log(count);
+console.log(++count);
+console.log(count--);
+console.log(count);
+console.log(--count);
+
+
+// Question 4
+// Predict the output first, then run the code.
+
+let x = 5;
+console.log(x++);
+// x=5, cuz post incre fisrtly print the value then add them.
+console.log(++x);
+// x=7, cuz pre incre directly adds them and then print the value.
+console.log(x);
+// x=7
+
+// Question 5
+// Find the output.
+
+console.log((10 + 5) * 2);
+// Answer=30
+console.log(20 - 10 / 2);
+// Answer=15
+console.log((20 - 10) / 2);
+// Answer=5
+
+// Question 6
+// Create two string variables.
+
+let firstName1 = "Hassan";
+let lastName1 = "Lashkarwala";
+console.log("Hello" +" "+ firstName1 +" "+ lastName1 );
+
+
+// Question 7
+// Use prompt().
