08e9b7067d5b69421407e6e8b78e75c24b251012
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-03 23:10:37 +0500

    JS assignment 6

---
+++ JS Assignment-06/app.js
@@ -0,0 +1,50 @@
+// Question 1
+
+let age = 20;
+let hasCNIC = true;
+if(age >= 18){
+    console.log( "yeah, you are adult!");
+    if(hasCNIC){
+        console.log("you are eligible to vote");
+        
+    }
+}
+
+// Question 2
+
+let userName = "AreebAli";
+let password = "areeb123";
+if(userName == "AreebAli"){
+    console.log("yes, your account is exist");
+    if(password == "areeb123"){
+        console.log("you are successfully loggedin");
+        
+    }
+}
+
+// Question 3
+
+let isLoggedin = true;
+let isAdmin = false;
+if(isLoggedin){
+    console.log("You are loggedin");
+    
+if(isAdmin){
+    console.log("Admin Panel open");
+    
+    }
+}
+
+// Question 4
+
+let marks = 90;
+let interview = true;
+if(marks >= 80){
+    console.log("Then check interview panel");
+    
+    if(interview){
+        console.log("congratulations! Admission confirmed.");
+        
+    }
+}
+
