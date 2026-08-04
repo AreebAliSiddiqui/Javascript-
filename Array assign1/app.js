0e7bb6ad3e13a95b5dead131a8d958c6f78c954d
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-04 22:16:35 +0500

    updated

---
+++ Array assign1/app.js
@@ -0,0 +1,18 @@
+// How to create an array?
+
+let cars = ["civic", "corolla", "grande", "fx", "khyber", "mehran" ]
+console.log(cars);
+// this method i can print any specific value from an array
+console.log(cars[2]);
+
+console.log(cars[3]);
+
+// this is how i can achnage any value from an array
+
+cars[2] = "BMW";
+
+console.log(cars[2]);
+
+console.log(cars);
+
+console.log(cars[5]);
