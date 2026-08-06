8957bbd0af6c1bf0070de50f57ab42cff30088c9
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-06 22:27:16 +0500

    Array methods

---
+++ Array methods/app.js
@@ -0,0 +1,16 @@
+// adding , removing and extracting elements of an array
+// Pop method
+
+// let cars = ["mehran", "cultus","alto","bmw","suzuki","khyber"]
+// console.log(cars);
+// cars.pop();
+// console.log(cars);
+
+// push method
+// let fruits =["mango","banana","apple","grapes","avacado","orange"]
+// console.log(fruits);
+// fruits.push("berrys","kivi")
+// console.log(fruits);
+
+
+// shift method
