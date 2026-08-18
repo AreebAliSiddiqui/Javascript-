81ecac82de4d5ac029c1da21cf1666ef6f946c2c
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-19 03:43:36 +0500

    Changing case

---
+++ Changing case/app.js
@@ -0,0 +1,23 @@
+// create different variables! Store value on this variable, string & arrays
+// and used method
+// toUpperCase()
+// toLowerCase()
+// .slice()
+// etc, covered chapter 21 & 22
+
+let students =["areeb","ALI","siddiqui"];
+console.log(students[2].toUpperCase());
+console.log(students[1].toLowerCase());
+console.log(students.slice(0,2));
+console.log(students.length);
+
+
+let country = "PAKistan";
+console.log(country.toUpperCase());
+console.log(country.toLowerCase());
+console.log(country.length);
+console.log(country.slice(0,4));
+console.log(country);
+
+
+
