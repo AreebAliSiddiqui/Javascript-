230a3bfd4ba7ea92c62f922b34d5f0f85dae2195
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-20 20:27:41 +0500

    chapter 23-24-25

---
+++ chapter 23-24-25/app.js
@@ -0,0 +1,49 @@
+// create variable let name = "Muhammad Hassan";
+// Find the position of "Hassan".
+
+let name ="Muhammad Hassan";
+console.log(name.indexOf("Hassan"));
+
+
+// create variable let city = "Karachi";
+// Print the character at index 2.
+
+let city = "Karachi";
+console.log(city.charAt(2));
+
+
+// create variable let text = "To be or not to be."
+// what's the length of this variable?
+
+let text = "To be or not to be.";
+console.log(text.length);
+
+
+// create variable let message = "I love CSS";
+// Replace "CSS" with "JavaScript".
+
+let message = "I love CSS";
+let newMessage = message.replace("CSS","Javascript")
+console.log(newMessage);
+
+
+// create variable let textEasy = "JavaScript is easy";
+// Find the position of "easy".
+
+let textEasy = "JavaScript is easy";
+console.log(textEasy.indexOf("easy"));
+
+
+// create variable let sentence = "I am learning HTML";
+// Replace "HTML" with "JavaScript" and print the new sentence.
+
+let sentence = "I am learning HTML";
+let newSentence =sentence.replace("HTML","Javascript");
+console.log(newSentence);
+
+// practice Qs
+
+let car ="Mehran khyber"
+console.log(car.indexOf("e"));
+console.log(car.lastIndexOf("e"));
+
