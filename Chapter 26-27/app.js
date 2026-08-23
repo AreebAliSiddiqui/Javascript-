5cbfeb970c433b659fcc5574056213ca5ab26b93
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-23 23:57:23 +0500

    updated

--- Chapter 26-27/app.js
+++ Chapter 26-27/app.js
@@ -10,5 +10,13 @@
 // Write a JavaScript program that stores 8.9 in a variable named number.
 // And print the result last integer in console.
 
-let number = 8.9;
-console.log(Math.floor(number));
+// let number = 8.9;
+// console.log(Math.floor(number));
+
+
+// Question 3
+// Write a JavaScript program that stores 5.2 in a variable named number.
+// And print the result upper integer in console
+
+let number = 5.2;
+console.log(Math.ceil(number));
