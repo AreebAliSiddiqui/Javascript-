df218144d65e71439898cd84898f0aaab8c948d4
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-23 23:50:50 +0500

    chapter 26-27

---
+++ Chapter 26-27/app.js
@@ -0,0 +1,6 @@
+// Question 1
+// Write a JavaScript program that stores 12.6 in a variable named number.
+// And print the rounded number in the console.
+
+let number = 12.6;
+console.log(number.rounded);
