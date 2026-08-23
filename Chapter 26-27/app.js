0ed16a53f12993e7007c08c4f47ea06a62ad31a6
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-23 23:55:03 +0500

    updated

--- Chapter 26-27/app.js
+++ Chapter 26-27/app.js
@@ -2,5 +2,13 @@
 // Write a JavaScript program that stores 12.6 in a variable named number.
 // And print the rounded number in the console.
 
-let number = 12.6;
-console.log(Math.round(number));
+// let number = 12.6;
+// console.log(Math.round(number));
+
+
+// Question 2
+// Write a JavaScript program that stores 8.9 in a variable named number.
+// And print the result last integer in console.
+
+let number = 8.9;
+console.log(Math.floor(number));
