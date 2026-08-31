64dc99fa51e638f99e841e53f8072bababdc1c4e
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-31 23:12:48 +0500

    updated

--- chapter 31-34/app.js
+++ chapter 31-34/app.js
@@ -5,3 +5,5 @@ console.log(typeof currentDate);
 currentDate = currentDate.toString();
 console.log(typeof currentDate);
 console.log(currentDate);
+// console.log(currentDate.getFullYear());
+console.log(currentDate.getMonth());
