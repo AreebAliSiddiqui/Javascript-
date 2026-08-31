2a96336a17c009d0d651ebc76f8008c516ad048c
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-08-31 23:08:55 +0500

    updated

--- chapter 31-34/app.js
+++ chapter 31-34/app.js
@@ -1,3 +1,7 @@
 
 let currentDate = new Date();
 console.log(currentDate);
+console.log(typeof currentDate);
+currentDate = currentDate.toString();
+console.log(typeof currentDate);
+console.log(currentDate);
