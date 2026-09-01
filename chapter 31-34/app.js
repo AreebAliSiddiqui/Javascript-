286d6b90d54754958f45eaa295349eec170a5162
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-02 03:56:11 +0500

    changes

--- chapter 31-34/app.js
+++ chapter 31-34/app.js
@@ -1,9 +1,22 @@
 
-let currentDate = new Date();
-console.log(currentDate);
-console.log(typeof currentDate);
-currentDate = currentDate.toString();
-console.log(typeof currentDate);
-console.log(currentDate);
-// console.log(currentDate.getFullYear());
-console.log(currentDate.getMonth());
+// Question 1
+// Create a variable named today.
+
+// let currentDate = new Date();
+// console.log(currentDate);
+// console.log(typeof currentDate);
+// currentDate = currentDate.toString();
+// console.log(typeof currentDate);
+
+
+// Question 2
+// Create a variable named activeYear and store the current date using new Date().
+// Print only the current year.
+
+let activeYear = new Date();
+console.log(activeYear.getFullYear());
+
+
+// Question 3
+// Create a variable named activeMonth.
+// Print only the current month.
