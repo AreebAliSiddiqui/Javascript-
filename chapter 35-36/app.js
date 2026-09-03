cefa5b083d47eed3ad1b4fb1ae78fe9c1ef76b3e
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-04 04:42:27 +0500

    chapter 35-36

---
+++ chapter 35-36/app.js
@@ -0,0 +1,14 @@
+// 1) Create simple function, function name myFunction. And console the value "Hello world!"
+
+function myFunction(){
+    console.log("Hello world!");
+    
+}
+myFunction();
+myFunction();
+myFunction();
+myFunction();
+myFunction();
+myFunction();
+myFunction();
+myFunction();
