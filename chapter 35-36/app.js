eae0bb37910cb7d755fc856ee8782cba2cb0fd9a
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-04 05:27:46 +0500

    updated

--- chapter 35-36/app.js
+++ chapter 35-36/app.js
@@ -16,10 +16,34 @@
 
 // 2) Create funtion, function name concatTwoWords, Used arguments and parameters and console of your name and your father name!
 
-function concatTwoWords(name,fatherName){
-    console.log("My name is " + name);
-    console.log("My father name is " + fatherName);
+// function concatTwoWords(name,fatherName){
+//     console.log("My name is " + name);
+//     console.log("My father name is " + fatherName);        
+// }
+// concatTwoWords("Areeb ali siddiqui","Mehmood ali siddiqui")
+
+
+// practice questions!
+
+// let students = ["areeb","ali","usman","usama"];
+// let subjects = ["Maths","science","physics","computer"];
+
+// function studentsInfo(studentsName,studentsSubject){
+//     console.log(studentsName);
+//     console.log(studentsSubject);
     
     
-}
-concatTwoWords("Areeb ali siddiqui","Mehmood ali siddiqui")
+// }
+// studentsInfo(students,subjects)
+
+
+// practice questions!
+// Write a function named convertToFahrenheit that takes a temperature in Celsius and converts it to Fahrenheit.
+
+// function convertToFahrenheit(Celsius){
+//     console.log("Tempterature in celsius is " + Celsius + " C");
+    
+//     console.log("Temperature in fahrenheit is " + ((Celsius * 1.8) + 32 )+ " F");
+    
+// }
+// convertToFahrenheit(25)
