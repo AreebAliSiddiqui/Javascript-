2a1a4ce93288954b804526b8a709e5bdb2ad0130
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-04 05:03:19 +0500

    updated

--- chapter 35-36/app.js
+++ chapter 35-36/app.js
@@ -1,14 +1,25 @@
 // 1) Create simple function, function name myFunction. And console the value "Hello world!"
 
-function myFunction(){
-    console.log("Hello world!");
+// function myFunction(){
+//     console.log("Hello world!");
+    
+// }
+// myFunction();
+// myFunction();
+// myFunction();
+// myFunction();
+// myFunction();
+// myFunction();
+// myFunction();
+// myFunction();
+
+
+// 2) Create funtion, function name concatTwoWords, Used arguments and parameters and console of your name and your father name!
+
+function concatTwoWords(name,fatherName){
+    console.log("My name is " + name);
+    console.log("My father name is " + fatherName);
+    
     
 }
-myFunction();
-myFunction();
-myFunction();
-myFunction();
-myFunction();
-myFunction();
-myFunction();
-myFunction();
+concatTwoWords("Areeb ali siddiqui","Mehmood ali siddiqui")
