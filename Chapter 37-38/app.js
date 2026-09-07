6ae525bd14351b499e2ea525da960e78874ae2e7
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-07 22:49:52 +0500

    chapter 37-38

---
+++ Chapter 37-38/app.js
@@ -0,0 +1,8 @@
+// 1) Create a function named userName that takes one parameter name, and take one argument value is "Hassan".
+// When the function is called, it should print: Hello Hassan
+
+function userName(name){
+    console.log(name);
+    
+}
+userName("Hassan");
