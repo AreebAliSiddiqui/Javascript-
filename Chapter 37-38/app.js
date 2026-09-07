46961db4b9df60f74493c22dd25fef2865ebc142
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-07 22:57:37 +0500

    updated

--- Chapter 37-38/app.js
+++ Chapter 37-38/app.js
@@ -1,18 +1,28 @@
 // 1) Create a function named userName that takes one parameter name, and take one argument value is "Hassan".
 // When the function is called, it should print: Hello Hassan
 
-function userName(name){
-    console.log(name);
+// function userName(name){
+//     console.log(name);
     
-}
-userName("Hassan");
+// }
+// userName("Hassan");
 
 
 // 2) Create a function named addWords that takes two parameters.
 // Pass "Hello" and "World" to the function and print boths word concat.
 
-function addWords(one,two){
-    console.log(one +" "+ two);
+// function addWords(one,two){
+//     console.log(one +" "+ two);
     
-}
-addWords("Hello","World")
+// }
+// addWords("Hello","World")
+
+
+// 3) Create a function named multiply that takes two parameters and returns their multiplication. Store the returned value in a variable and print it.
+
+// function multiply(value1,value2){
+    
+//     return value1 * value2
+// }
+// let result = multiply(5,8);
+// console.log(result);
