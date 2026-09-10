de924f623626425238a49eb53b4aab94fa3ce529
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-11 04:06:50 +0500

    changes

--- chapter 39-40/app.js
+++ chapter 39-40/app.js
@@ -16,3 +16,20 @@
 //     default :
 //     console.log("No day!!");
 // }
+
+
+let myCar =prompt("Enter yoyr car name");
+
+switch (myCar){
+    case "Mehran" :
+    console.log("nice");
+    break;
+
+    case "corolla":
+    console.log("nice car");
+    break;
+
+    case "Alto":
+    console.log("Good");
+    break;    
+}
