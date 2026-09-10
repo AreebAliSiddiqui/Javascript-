d0b9eb3d34c07908e11370bdbcc9cb02b0804a60
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-11 04:01:35 +0500

    chapter 39-40

--- Chapter 37-38/app.js
+++ Chapter 37-38/app.js
@@ -106,9 +106,4 @@
 // console.log(message);// it gives a undefined error becuz message variable is declare inside a function so it can only be access inside not outside.
 // message is a local variable cuz it is used inside a function of block
  
-function callMyFunction(){
-    let localVarible = "Hey Local Varible!!";
-    console.log(localVarible);
-}
-callMyFunction()
-console.log(localVarible);
+ 
---
+++ chapter 39-40/app.js
@@ -0,0 +1,18 @@
+// let dayOfWeek = "Saturday";
+
+// switch(dayOfWeek){
+//     case "Saturday":
+//         console.log("Today is Saturday");
+//          break;
+
+//     case "Sunday":
+//         console.log("Today is Sunday, Funday");
+//          break;
+
+//     case "Monday":
+//         console.log("Today is monday, Working day");   
+//         break; 
+
+//     default :
+//     console.log("No day!!");
+// }
