6dd349e7f6110ed344b5b0fc163913b92287b0e5
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-13 20:38:22 +0500

    changes

--- chapter 39-40/app.js
+++ chapter 39-40/app.js
@@ -46,12 +46,25 @@
 //     i++;
 // }
 
-function print(number){
-    let i = 1;
-    while(i <= number){
-        console.log(i);
-    i++;    
-    }
+// function print(number){
+//     let i = 1;
+//     while(i <= number){
+//         console.log(i);
+//     i++;    
+//     }
     
+// }
+// print(10)
+
+
+let sum = 0;
+function add(number){
+    let i = 1;
+    while (i <= number){
+        sum = sum + i;
+    i++;
+    }
+return sum;
 }
-print(10)
+let result = add(10);
+console.log(result);
