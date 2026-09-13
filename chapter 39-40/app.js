065939ac2544bd40bf8782cfc10480d72bf39056
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-13 20:34:51 +0500

    while loop practice

--- chapter 39-40/app.js
+++ chapter 39-40/app.js
@@ -33,3 +33,25 @@
 //     console.log("Good");
 //     break;    
 // } 
+
+
+//------------------------------------ While loop---------------------------------------//
+
+// let i = 1;
+// while(i <= 20){
+//     if(i % 2 == 0){
+//         console.log(i);
+        
+//     }
+//     i++;
+// }
+
+function print(number){
+    let i = 1;
+    while(i <= number){
+        console.log(i);
+    i++;    
+    }
+    
+}
+print(10)
