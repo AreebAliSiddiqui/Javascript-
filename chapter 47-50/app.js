bd1f5046b1daadebc3ce319202630e9ff121298c
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-24 05:13:27 +0500

    Chapter 45-50

---
+++ chapter 47-50/app.js
@@ -0,0 +1,18 @@
+function submitForm(){
+    let userName = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let age = document.getElementById("age").value;
+    age = Number(age);
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+    let city = document.getElementById("City").value;
+    
+    console.log("userName : " + userName);
+    console.log("email : " + email);
+    console.log("password : " + password);
+    console.log("age : " + age);
+    console.log("gender : " + gender);
+    console.log("city : " + city);
+    
+
+}
