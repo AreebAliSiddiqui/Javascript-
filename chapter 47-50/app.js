56f65fbf44995d8fc48155a53fdcb4a38d4812be
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-09-24 22:57:21 +0500

    updated

--- chapter 47-50/app.js
+++ chapter 47-50/app.js
@@ -1,4 +1,4 @@
-function submitForm(){
+function submitForm() {
     let userName = document.getElementById("username").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -6,13 +6,13 @@ function submitForm(){
     age = Number(age);
     let gender = document.querySelector('input[name="gender"]:checked').value;
     let city = document.getElementById("City").value;
-    
+
     console.log("userName : " + userName);
     console.log("email : " + email);
     console.log("password : " + password);
     console.log("age : " + age);
     console.log("gender : " + gender);
     console.log("city : " + city);
-    
+
 
 }
